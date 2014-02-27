1bc0222d16d6e053a313f73ad4019035b205c468
Gregg Horton and Andrew Richards <drew.rchrds@gmail.com>
2014-02-27 10:11:36 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BetterReddit::Application.config.secret_key_base = 'd3a2e613bb4bd1511347a2dfdddbc0f35436fcbb5f54488437cdbc024c1c0117b524b75046eec19b62c241230e5da38f5bb5e51b8689742c9827da401c86e7f5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BetterReddit::Application.config.session_store :cookie_store, key: '_BetterReddit_session'
